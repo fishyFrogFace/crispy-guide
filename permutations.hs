c898c7bb709c7e3161f2c64b9ba36052521f2772
Camilla Rygh <camillahallo@mail.com>
2018-12-31 05:18:26 +0100

    Permutations with repeated elements

--- permutations.hs
+++ permutations.hs
@@ -10,3 +10,12 @@ factorial n = factorials !! n
 -- n >= k
 kPermutationsOfN :: Int -> Int -> Integer
 kPermutationsOfN n k = div (factorial n) (factorial $ n-k)
+
+-- Permutations of n objects where one or more are repeated,
+-- e.g. permutations of the word AARDWARK
+repetitions :: Int -> [Int] -> Integer
+repetitions n reps = factorial n `div` rep reps
+                      where
+                     rep :: [Int] -> Integer
+                     rep []     = 1
+                     rep (x:xs) = factorial x + rep xs
